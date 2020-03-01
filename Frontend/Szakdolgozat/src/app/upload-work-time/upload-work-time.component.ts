a63937045784ee6d29e3655751b93de637afbbd0
Kotroczó Roland <k.roli9955@gmail.com>
2020-03-01 16:21:41 +0100

    Upload work-time panel component init

---
+++ Frontend/Szakdolgozat/src/app/upload-work-time/upload-work-time.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-work-time',
+  templateUrl: './upload-work-time.component.html',
+  styleUrls: ['./upload-work-time.component.css']
+})
+export class UploadWorkTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
